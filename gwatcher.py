ee872e1dad160dfe3d5873d113bd6c3cad781243
Volfar <bogdanvennyk@gmail.com>
2017-12-10 12:56:55 +0200

    GWatcher v2.0
    Improved perfomance.
    Added multiprocessing with Pool. Removed overlap with ListDrive function.
    Recursive method of iteration replaced with iterative.
    
    Added output progress and total time.

--- gwatcher.py
+++ gwatcher.py
@@ -2,6 +2,10 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import csv
 import os
+from multiprocessing import Manager, Queue, Pool
+from datetime import datetime
+import time
+
 
 gauth = GoogleAuth()
 gauth.LoadCredentialsFile("mycreds.txt")
@@ -15,85 +19,91 @@ gauth.SaveCredentialsFile("mycreds.txt")
 drive = GoogleDrive(gauth)
 
 
-def write_results(perm, watch, emailAddress):
-    if str(perm.get("type")) == 'anyone':
-        watch['anyone'] = str(perm.get("role"))
-    elif str(perm.get("type")) == 'user':
-        if emailAddress not in fieldnames:
-            fieldnames.append(emailAddress)
-            writer = csv.DictWriter(gwatcher, fieldnames=fieldnames)
-            watch[emailAddress] = str(perm.get("role"))
+def writeResults(file):
+    if file["mimeType"] == "application/vnd.google-apps.folder":
+        folders.append(file["id"])
+        if file["parents"]:
+            parent_dir[file["id"]] = (parent_dir[file["parents"][0]["id"]] + str(file["title"] + "/"))
         else:
-            watch[emailAddress] = str(perm.get("role"))        
-    return watch
-
-
-def lets_godeeper(id,writer,watch, level):
-    for file in drive.ListFile({'q': "'"+id+"' in parents"}).GetList():
-        for perm in file.GetPermissions():
-            if perm.get('emailAddress') is not None:
-                emailAddress = str(perm.get("emailAddress"))
+            parent_dir[file["id"]] = ("/" + str(file["title"] + "/"))
+    watch = {}
+    for perm in file.GetPermissions():
+        if perm.get('emailAddress') is not None:
+            emailAddress = str(perm.get("emailAddress"))
+        else:
+            emailAddress = ""
+        if str(perm.get("type")) == 'anyone':
+            watch['anyone'] = str(perm.get("role"))
+        elif str(perm.get("type")) == 'user':
+            if emailAddress not in fields:
+                fields.append(emailAddress)
+                watch[emailAddress] = str(perm.get("role"))
             else:
-                emailAddress = ""
-            write_results(perm, watch, emailAddress)
-        watch['Path'] = ("/" + str(level) + str(file["title"]))
-        watch['FileType'] = str(file["mimeType"].split(".")[-1])
-        watch['Owner'] = str(file["ownerNames"][0])
-        watch['CreatedDate'] = str(file["createdDate"])
-        watch['ModifiedDate'] = str(file["modifiedDate"])
-        watch['AlternateLink'] = str(file["alternateLink"])
-        writer.writerow(watch)
-        watch.clear()
-        if file["mimeType"] == "application/vnd.google-apps.folder":
-            lets_godeeper(file["id"],writer, watch,level + file['title'] + "/")
+                watch[emailAddress] = str(perm.get("role"))
+    if not file["parents"]:
+         watch['Path'] = ("/" + str(file["title"]))
+    else:
+         watch['Path'] = (str(parent_dir[file["parents"][0]["id"]]) + str(file["title"]))
+    watch['FileType'] = str(file["mimeType"].split(".")[-1])
+    watch['Owner'] = str(file["ownerNames"][0])
+    watch['CreatedDate'] = str(file["createdDate"])
+    watch['ModifiedDate'] = str(file["modifiedDate"])
+    watch['AlternateLink'] = str(file["alternateLink"])
+    q.put(watch)
+    progress.append("0")
+    print("Total files checked:" + str(len(progress)))
 
 
-
-def file_enum(writer, query):
-    root_dir = drive.ListFile(
-    {'q': query}).GetList()
-
-    for root_file in root_dir:
-        watch = {}
-        for perm in root_file.GetPermissions():
-            if perm.get('emailAddress') is not None:
-                emailAddress = str(perm.get("emailAddress"))
-            else:
-                emailAddress = ""
-            write_results(perm, watch, emailAddress)
-        watch['Path'] = ("/" + str(root_file["title"]))
-        watch['FileType'] = str(root_file["mimeType"].split(".")[-1])
-        watch['Owner'] = str(root_file["ownerNames"][0])
-        watch['CreatedDate'] = str(root_file["createdDate"])
-        watch['ModifiedDate'] = str(root_file["modifiedDate"])
-        watch['AlternateLink'] = str(root_file["alternateLink"])
-        writer.writerow(watch)
-        watch.clear()
-        if root_file["mimeType"] == "application/vnd.google-apps.folder":
-            lets_godeeper(root_file["id"],writer,watch, root_file['title'] + "/")
-
-
-            
-
-
-with open('tmp.csv', 'w') as gwatcher:
+if __name__ == '__main__':
+    startTime = datetime.now()
+    gwatcher = open('tmp.csv', 'w')
     fieldnames = ['Path', 'FileType', 'Owner', 'CreatedDate',
-                  'ModifiedDate',  'AlternateLink', 'anyone']
+                  'ModifiedDate', 'AlternateLink', 'anyone']
     writer = csv.DictWriter(gwatcher, fieldnames=fieldnames)
     writer.writeheader()
-    file_enum(writer,"'root' in parents and trashed=false")
-    file_enum(writer, "sharedWithMe=True")
+    q = Queue()
+    manager = Manager()
+    folders = manager.list()
+    fields = manager.list()
+    progress = manager.list()
+    parent_dir = manager.dict()
+    files = drive.ListFile({'q':'"root" in parents'}).GetList()
+    pool = Pool()
+    parent_dir[files[0]['parents'][0]["id"]] = "/"
+    p = pool.map_async(writeResults, files)
+    p.wait()
+    filesShared = drive.ListFile({'q': 'sharedWithMe'}).GetList()
+    p = pool.map_async(writeResults, filesShared)
+    p.wait()
+    while folders:
+        time.sleep(0.1)
+        for id in folders:
+            time.sleep(0.1)
+            files = drive.ListFile({'q': "'" + str(id) + "' in parents"}).GetList()
+            p = pool.map_async(writeResults, files)
+            folders.remove(id)
+    p.wait()
+
+    for field in fields:
+        if field not in fieldnames:
+            fieldnames.append(field)
+
+    while not q.empty():
+        writer.writerow(q.get())
+
+    q.close()
+    q.join_thread()
     writer.writeheader()
     gwatcher.close()
+    print(datetime.now()-startTime)
+    with open("tmp.csv", "r") as file:
+        lines = file.readlines()
+        lines[0] = lines[-1]
+        lines = lines[:-1]
+        file.close()
 
-with open("tmp.csv", "r") as file:
-    lines = file.readlines()
-    lines[0] = lines[-1]
-    lines = lines[:-1]
-    file.close()
-
-with open("gwatcher.csv", "w") as file:
-    for line in lines:
-        file.write(line)
-    file.close()
-    os.remove("tmp.csv")
+    with open("gwatcher.csv", "w") as file:
+        for line in lines:
+            file.write(line)
+        file.close()
+        os.remove("tmp.csv")
